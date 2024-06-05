94bb8db2852b4ed54fb604528dda695ec74a9139
danielRamirezTomas <daniel.ramirez.tomas@gmail.com>
2024-06-05 15:24:58 +0200

    db fix

--- src/controllers/index.controller.js
+++ src/controllers/index.controller.js
@@ -1,19 +1,13 @@
 import {pool} from "../db.js";
-import {
-  DB_HOST,
-  DB_PORT,
-  DB_USER,
-  DB_PASSWORD,
-  DB_DATABASE,
-} from "./config.js";
-
+import { config } from "dotenv";
+config();
 export const ping = async (req, res) => {
   try {
     const [result] = await pool.query('SELECT "Pong" AS result');
     res.json(result[0]);
   } catch (error) {
     return res.status(500).json({
-      message: "Something went wrong - " + "HOST " + DB_HOST + " PORT " + DB_PORT + " USER " + DB_USER + " PASSWORD " + DB_PASSWORD + " DATABASE " + DB_DATABASE,
+      message: "Something went wrong - " + process.env.PORT + " -" +process.env.DB_HOST + " -" +process.env.DB_PORT + " -" +process.env.DB_USER + " -" +process.env.DB_PASSWORD + " -" +process.env.DB_DATABASE
     });
   }
 };
