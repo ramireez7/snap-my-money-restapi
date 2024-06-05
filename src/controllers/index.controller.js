59a2be9d75d5d1776a47901abf036b65541c0096
danielRamirezTomas <daniel.ramirez.tomas@gmail.com>
2024-06-05 15:12:22 +0200

    debug

--- src/controllers/index.controller.js
+++ src/controllers/index.controller.js
@@ -1,4 +1,11 @@
 import {pool} from "../db.js";
+import {
+  DB_HOST,
+  DB_PORT,
+  DB_USER,
+  DB_PASSWORD,
+  DB_DATABASE,
+} from "./config.js";
 
 export const ping = async (req, res) => {
   try {
@@ -6,7 +13,7 @@ export const ping = async (req, res) => {
     res.json(result[0]);
   } catch (error) {
     return res.status(500).json({
-      message: "Something went wrong - " + error.message,
+      message: "Something went wrong - " + "HOST " + DB_HOST + " PORT " + DB_PORT + " USER " + DB_USER + " PASSWORD " + DB_PASSWORD + " DATABASE " + DB_DATABASE,
     });
   }
 };
